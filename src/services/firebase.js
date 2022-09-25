b58c1b29204da55e0a8995830d712aebf1725722
tsukazun <112553793+tsukazun@users.noreply.github.com>
2022-09-25 18:05:30 +0200

    Update firebase.js

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,12 +3,13 @@ import "firebase/compat/auth";
 import "firebase/compat/firestore";
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+  apiKey: "AIzaSyDRLFt2-6R-Qulv9teAvDCwc7GQBT8wMyY",
+  authDomain: "twitter-clone-781b6.firebaseapp.com",
+  projectId: "twitter-clone-781b6",
+  storageBucket: "twitter-clone-781b6.appspot.com",
+  messagingSenderId: "917187045833",
+  appId: "1:917187045833:web:12d53ed87e4fd68e8c6018",
+  measurementId: "G-NNJ5XRQVV2"
 };
 
 const app = firebase.initializeApp(firebaseConfig);
